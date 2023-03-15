9f9406d7f6a4bf6ebb79cc920872735b24175dbc
Supakorn <supakorn0728@gmail.com>
2023-03-15 15:48:23 +0700

    initial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <main>
-            <h1 className="text-lg">Hello World</h1>
+            <h1 className="text-lg">Hello World!!</h1>
         </main>
     );
 }
