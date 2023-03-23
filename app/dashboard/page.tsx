dd714d5102de1b540880ea7663de95f0a2e2eda8
Supakorn <supakorn0728@gmail.com>
2023-03-23 21:13:40 +0700

    initial

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        redirect("/api/auth/signin");
+    }
+
+    return (
+        <main>
+            <h1 className="text-2xl font-bold">Welcome back {session?.user?.name}</h1>
+        </main>
+    );
+}
